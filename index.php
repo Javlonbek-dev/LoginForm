a284db290c9d3e2304e674e7cf9068ebb4bc5184
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-10-17 17:34:14 +0500

    Add: Some changes at index.php

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="login.php" method="post">
+        <h2>LOGIN</h2>
+        <?php if(isset( $_POST['error'])){?>
+            <p class="error"> <?php echo $_POST['error'];?></p>
+            <?php } ?>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name">
+        <label>Password</label>
+        <input type="number" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
